307ed4a6bb0e35887afba3fa04da4a533ba15a0e
Jens de Bruijn <j.a.debruijn@outlook.com>
2021-12-07 14:26:55 +0100

    fix

--- efficientnet/initializers.py
+++ efficientnet/initializers.py
@@ -41,7 +41,7 @@ class EfficientConv2DKernelInitializer(Initializer):
     def __call__(self, shape, dtype=K.floatx(), **kwargs):
         kernel_height, kernel_width, _, out_filters = shape
         fan_out = int(kernel_height * kernel_width * out_filters)
-        return tf.random_normal(
+        return tf.random.normal(
             shape, mean=0.0, stddev=np.sqrt(2.0 / fan_out), dtype=dtype
         )
 
